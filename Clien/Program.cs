620b2415b579c55f2e5c20c86e0cd8a9fca3486c
Jola <vjola@internal.tavisca.com>
2019-07-30 14:57:51 +0530

    Add project files.

---
+++ Clien/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            string ipaddress = "172.16.5.173";
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipaddress), port);
+            //Thread t1 = new Thread(new ThreadStart());
+
+            clientSocket.Connect(ep);
+            Console.WriteLine("CLient is connected");
+            while(true)
+            {
+                string msg = null;
+                Console.WriteLine("Enter message");
+                msg = Console.ReadLine();
+                clientSocket.Send(System.Text.Encoding.ASCII.GetBytes(msg), 0, msg.Length,SocketFlags.None);
+                byte[] msgfromServer = new Byte[100];
+                int size=clientSocket.Receive(msgfromServer);
+
+                Console.WriteLine("server" + System.Text.Encoding.ASCII.GetString(msgfromServer,0,size));
+
+
+            }
+
+        }
+    }
+}
